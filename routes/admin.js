dfb08f6a1e9b10fd0c7e5c3f960e5f337ac338b1
GiovaniMSJ <140211502+GiovaniMSJ@users.noreply.github.com>
2025-02-14 20:52:10 -0300

    deletar categorias
    Foi criado  a função de deletar as categorias

--- routes/admin.js
+++ routes/admin.js
@@ -127,5 +127,16 @@ router.post('/categorias/edit', (req, res) => {
     }
 })
 
+// Rota de deletar categoria
+router.post('/categorias/deletar',(req, res) => {
+    Categoria.deleteOne({_id: req.body.id}).then(() => {
+        req.flash('success_msg', 'Categoria deletada com sucesso!')
+        res.flash('/admin/categorias')
+    }).catch((e) => {
+        req.flash('error_msg' , 'Houve um erro ao deletar a categoria')
+        res.redirect('/admin/categorias')
+    })
+})
+
 // Exportando as rotas
 module.exports = router
